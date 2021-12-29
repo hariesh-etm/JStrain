63557c8218a1f49c0e1136a87b4306096f68c039
hariesh-etm <95334384+hariesh-etm@users.noreply.github.com>
2021-12-29 14:36:17 +0530

    Update app.js

--- D18_ClosuresAndCallbacks/Finished/app.js
+++ D18_ClosuresAndCallbacks/Finished/app.js
@@ -6,21 +6,21 @@ function sayHiLater() {
         
         console.log(greeting);
         
-    }, 3000);
+    }, 1000);
     
 }
 
 sayHiLater();
 
 // jQuery uses function expressions and first-class functions!
-//$("button").click(function() {
-//    
-//});
+// $("button").click(function() {
+   
+// });
 
 function tellMeWhenDone(callback) {
  
-    var a = 1000; // some work
-    var b = 2000; // some work
+    var a = 8000; // some work
+    var b = 9000; // some work
     
     callback(); // the 'callback', it runs the function I give it!
     
